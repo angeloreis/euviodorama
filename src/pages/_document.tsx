a0288a5a56b3b1f392073a956ccae34a3aa2637b
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-08-18 17:31:21 -0300

    fix: import style external

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,17 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/img/favicon.png" />
           <link rel="apple-touch-icon" href="/img/favicon.png" />
           <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
           <meta name="theme-color" content="#ff8000" />
           <meta
             name="description"
