ea7fbd32e1e4623b150bc3de55698e132053bf29
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-22 01:49:15 -0300

    fix: new site of membership

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
 import Head from 'next/head'
+import { Header } from 'components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,7 +10,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <title>Eu Vi o dorama</title>
         </Head>
-        <Component {...pageProps} />
+        <Flex flexDirection="column" alignItems="center" background="orange.400" color="blackAlpha.900">
+          <Header />
+          <Component {...pageProps} />
+          <Flex as="footer" flexDirection="column" alignItems="center" justify="center" paddingY='35px' paddingX="10px" w="100%" color="white" background="orange.700">
+            <div>Angelo Reis &copy; Todos os direitos reservados - 2009/2022</div>
+          </Flex>
+        </Flex>
       </ChakraProvider>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,45 @@
-import { Flex } from '@chakra-ui/react'
-// import { BlogSection } from 'components/BlogSection'
-import { Header } from 'components/Header'
-import { HowStarted } from 'components/Sections/HowStarted'
-import { ImageSlider } from 'components/Sections/ImageSlider'
-import { Main } from 'components/Main'
+import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react'
 
-interface HomeProps {
-  maintenance: boolean
-}
-
-export default function Home({ maintenance }: HomeProps) {
-  if (maintenance) {
-    return <Main />
-  }
-
+export default function Home() {
   return (
-    <Flex direction="column" width="100vw" background='black' height='100vh'>
-      <Header />
-      <ImageSlider />
-      <HowStarted />
+    <Flex
+      as="main"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      w="100%"
+      maxWidth="1120px"
+      marginY="0"
+      marginX={["1rem", "auto"]}
+      height='calc(100vh - 5rem)'
+      color="white">
+      <Flex flexDir={["column", "row"]} marginX={["0.875rem", "auto"]} marginTop={['4rem','5rem']}>
+        <Flex as="section" maxWidth="600px" flexDir="column">
+          <Text as="span" fontSize={["1rem", "1.5rem"]} fontWeight="bold">안녕하세요 (annyeonghaseyo) 🇰🇷</Text>
+          <Heading fontSize={["1.25rem", "4.5rem"]} lineHeight={["1.25rem", "4.5rem"]} fontWeight='extrabold' marginTop={["0.75rem", "2.5rem"]}>Aqui você tem a melhor resenha dos doramas em evidência</Heading>
+          <Text fontSize={["0.875rem", "1.25rem"]} lineHeight="1.25rem" marginY={["1rem", "1.5rem"]}>
+            Resenhas com link para os episódios? <br />
+            Por apenas R$ 10/mês tenha acesso a minha lista de curadoria
+          </Text>
+          <Flex alignItems={["center", "flex-start"]} justifyContent={["center", "flex-start"]}>
+            <Button
+              height="4rem"
+              width="260px"
+              border="0"
+              borderRadius="2rem"
+              background="orange.900"
+              color="white"
+              fontSize="1.25rem"
+              transition="filter 0.2s"
+              marginBottom={["0.5rem", "1.5rem"]}
+              _hover={{
+                background: "orange.500",
+                filter: 'brightness(0.8)'
+              }}>Assinar Membro</Button>
+          </Flex>
+        </Flex>
+        <Image src="./img/hero.svg" height={["356px", "500px"]} />
+      </Flex>
     </Flex>
   )
 }
