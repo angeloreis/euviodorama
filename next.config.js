1e5a5ff205febaaf47b47b394645d12719ffd65f
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-21 22:25:20 -0300

    fix: update nextjs, deps and tsconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['github.com'],
+  },
 }
 
 module.exports = nextConfig
