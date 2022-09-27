545e016ecaf595b23f4b06d23424cf12afea0801
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-26 22:29:54 -0300

    fix: loading info ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react'
+import { getPlanRecommend } from 'services/plans'
+import { formattedPrice } from 'utils/formatted'
 
-export default function Home() {
+interface HomeProps {
+  priceFormatted: string
+}
+
+export default function Home({ priceFormatted }: HomeProps) {
   return (
     <Flex
       as="main"
@@ -13,27 +19,31 @@ export default function Home() {
       marginX={["1rem", "auto"]}
       height='calc(100vh - 5rem)'
       color="white">
-      <Flex flexDir={["column", "row"]} marginX={["0.875rem", "auto"]} marginTop={['4rem','5rem']}>
+      <Flex flexDir={["column", "row"]} marginX={["0.875rem", "auto"]} marginTop={['4rem', '5rem']}>
         <Flex as="section" maxWidth="600px" flexDir="column">
           <Text as="span" fontSize={["1rem", "1.5rem"]} fontWeight="bold">안녕하세요 (annyeonghaseyo) 🇰🇷</Text>
           <Heading fontSize={["1.25rem", "4.5rem"]} lineHeight={["1.25rem", "4.5rem"]} fontWeight='extrabold' marginTop={["0.75rem", "2.5rem"]}>Aqui você tem a melhor resenha dos doramas em evidência</Heading>
           <Text fontSize={["0.875rem", "1.25rem"]} lineHeight="1.25rem" marginY={["1rem", "1.5rem"]}>
             Resenhas com link para os episódios? <br />
-            Por apenas R$ 10/mês tenha acesso a minha lista de curadoria
+            Por apenas <strong>{priceFormatted}</strong> tenha acesso a minha lista de curadoria
           </Text>
           <Flex alignItems={["center", "flex-start"]} justifyContent={["center", "flex-start"]}>
             <Button
               height="4rem"
               width="260px"
-              border="0"
+              border="1px"
+              borderColor='orange.200'
               borderRadius="2rem"
-              background="orange.900"
+              background="orange.500"
               color="white"
               fontSize="1.25rem"
               transition="filter 0.2s"
               marginBottom={["0.5rem", "1.5rem"]}
               _hover={{
-                background: "orange.500",
+                fontSize: "1.45rem",
+                color: "white",
+                background: "orange.600",
+                borderColor: 'orange.900',
                 filter: 'brightness(0.8)'
               }}>Assinar Membro</Button>
           </Flex>
@@ -42,4 +52,15 @@ export default function Home() {
       </Flex>
     </Flex>
   )
+}
+
+
+export async function getServerSideProps() {
+  const plan = await getPlanRecommend()
+  const priceFormatted = plan && formattedPrice(plan.price) || ''
+  return {
+    props: {
+      priceFormatted
+    }
+  }
 }
