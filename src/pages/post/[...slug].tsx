34bb26b91f5dbb78569c0a68befea9c522b2afe8
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-26 22:44:16 -0300

    fix: eslint config

--- src/pages/post/[...slug].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function Post() {
-  const router = useRouter()
-  const slug = router.query
-  return <>oie Post {slug}</>
- }
--- src/pages/post/preview/[...slug].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function PreviewPost() {
-  const router = useRouter()
-  const slug = router.query
-  return <>oie Preview Post {slug}</>
- }
