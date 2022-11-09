c19b45e16ea9728bfa3e327f28a63ec0a1598833
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-11-09 17:36:08 -0300

    feat: add new queries and api types

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -33,5 +33,37 @@ export const GET_POST_BY_SLUG = gql`
   }
 `
 
+export const GET_CATEGORIES = gql`
+  query getCategories {
+    categories {
+      name
+      slug
+    }
+  }
+`
 
+export const GET_CATEGORY_BY_SLUG = gql`
+  query getCategoryBySlug($slugCategory: String!) {
+    category(where: {slug: $slugCategory}) {
+      name
+      slug
+    }
+  }
+`
 
+export const GET_POST_BY_CATEGORY = gql`
+  query getPostByCategory($categorySlug: String!) {
+    posts(where: { categories_some: { slug: $categorySlug }} ){
+      id
+      slug
+      title
+      heading
+      imageFeature {
+        url
+      }
+      body {
+        html
+      }
+    }
+  }
+`
---
+++ src/types/api.ts
@@ -0,0 +1,13 @@
+export type CategoryProps = {
+  name: string
+  slug: string
+}
+
+export type PostProps = {
+  slug: string
+  title: string
+  imageFeature: {
+    url: string
+  }
+  heading: string
+}
