bea324bad112cd5597101624a808ee9f41746132
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-11-09 17:37:13 -0300

    feat: add filter buttons to select category

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,20 +1,16 @@
-import { Flex, Divider } from "@chakra-ui/react";
+import { Flex, Divider, Button, Wrap, WrapItem } from "@chakra-ui/react";
 import client from "graphql/client";
-import { GET_POSTS } from "graphql/queries";
+import { GET_CATEGORIES, GET_POSTS } from "graphql/queries";
 import { ItemPost } from "components/Post/Item";
-interface Post {
-  slug: string
-  title: string
-  imageFeature: {
-    url: string
-  }
-  heading: string
-}
+import { CategoryProps, PostProps } from "types/api";
+import Link from "next/link";
+
 interface BlogProps {
-  posts: Post[]
+  posts: PostProps[]
+  categories: CategoryProps[]
 }
 
-export default function Blog({ posts }: BlogProps) {
+export default function Blog({ posts, categories }: BlogProps) {
   return (
     <Flex
       id="app-blog"
@@ -26,6 +22,39 @@ export default function Blog({ posts }: BlogProps) {
       margin="32px"
       color='white'>
       <Flex flexDir="column" width="100%" marginLeft="6px" marginTop="16px" marginBottom="16px" padding="16px">
+        {categories ? (
+          <Flex py="1rem"
+            maxWidth="980px"
+            minWidth="319px">
+            <Wrap spacing={3}>
+              {
+                categories.map((category) => {
+                  return (
+                    <WrapItem key={category.name}>
+                      <Link href={`/blog/category/${category.slug}`}>
+                        <Button
+                          bg="orange.500"
+                          minW="8rem"
+                          maxW="15rem"
+                          width="100%"
+                          _hover={{
+                            bg: "orange.600",
+                            borderColor: "white"
+                          }}
+                          cursor="pointer"
+                          border="1px"
+                          borderColor="orange.600">
+                          {category.name}
+                        </Button>
+                      </Link>
+                    </WrapItem>
+                  )
+                })
+              }
+            </Wrap>
+          </Flex>
+        ) : null}
+
         {posts && posts.map(({ slug, title, heading, imageFeature }, index) => (
           <>
             <ItemPost key={index} slug={slug} title={title} heading={heading} url={imageFeature.url} />
@@ -39,10 +68,12 @@ export default function Blog({ posts }: BlogProps) {
 
 export async function getStaticProps() {
   const { posts } = await client.request(GET_POSTS)
+  const { categories } = await client.request(GET_CATEGORIES)
 
   return {
     props: {
-      posts
+      posts,
+      categories
     }
   }
 }
