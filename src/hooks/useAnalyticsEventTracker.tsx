47b95e592b9a5e6fef57301d29df32fbfddd1b44
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-12-05 14:28:26 -0300

    fix: delete hook react-ga

--- src/hooks/useAnalyticsEventTracker.tsx
+++
@@ -1,9 +0,0 @@
-import ReactGA from 'react-ga'
-
-export const useAnalyticsEventTracker = (category = 'Home') => {
-  const eventTracker = (action = 'enter home', label = 'Access Home Page') => {
-    return ReactGA.event({category, action, label})
-  }
-
-  return eventTracker
-}
